136f58e96b82a283324d2a714a68b51a6d666ebe
Josannnnn <Jonathanss999@gmail.com>
2024-11-20 19:23:37 +0700

    1
    1

---
+++ Belajar_angular_jonathan/express-mvc/app_server/models/db.js
@@ -0,0 +1,23 @@
+let mongoose = require("mongoose");
+let dbURI = "mongodb+srv://paw2:si@paw2.iendmj6.mongodb.net/PAWII-SI?retryWrites=true&w=majority&appName=paw2";
+
+mongoose.connect(dbURI,{
+    //useNewURLParser: true
+});
+
+mongoose.connection.on("connected", () => {
+    console.log("Connected To MongoDB");
+});
+
+mongoose.connection.on("error", (error) => {
+    console.log("Connection Error: " + error);
+});
+
+mongoose.connection.on("disconected", () => {
+    console.log("Disconnected From MongoDB");
+});
+
+<<<<<<< HEAD
+require("./mahasiswa");
+=======
+>>>>>>> 9a56162b6714c6652dce5f94d541eb2fab779a0b
